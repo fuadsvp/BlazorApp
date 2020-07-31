6e4e6f22a653bf3ddafad8331d7152e378aa5a68
fuadsvp <fuadsvp@gmail.com>
2020-07-31 20:18:08 +0530

    initial commit

---
+++ BlazorCRUD/Data/EmployeeService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Data
+{
+    public class EmployeeService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EmployeeService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //for CRUD operation
+
+        //Get All Employees
+
+        public List<EmployeeInfo> GetEmployee()
+        {
+            var empList = _db.Employees.ToList();
+            return empList;
+        }
+
+        //Insert employee info
+
+        public string Create(EmployeeInfo objEmployee)
+        {
+            _db.Employees.Add(objEmployee);
+            _db.SaveChanges();
+            return "Save Succefully";
+        }
+
+        //Get Employee By ID
+
+        public EmployeeInfo GetEmployeeById(int id)
+        {
+            EmployeeInfo employee = _db.Employees.FirstOrDefault(s => s.EmployeeId == id);
+            return employee;
+
+        }
+
+        //Update Employee Info
+        public string UpdateEmployee(EmployeeInfo objEmployee)
+        {
+            _db.Employees.Update(objEmployee);
+            _db.SaveChanges();
+            return "Update Successfully";
+        }
+
+        //Delete  Employee
+        public string DeleteEmpInfo(EmployeeInfo objEmployee)
+        {
+            _db.Remove(objEmployee);
+            _db.SaveChanges();
+            return "Delete Successfully";
+        }
+    }
+}
